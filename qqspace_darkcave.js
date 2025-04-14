86ec05b4e941687726e46548f9189074f0ff91fb
songxin <songxinxinxx@gmail.com>
2025-04-14 14:04:39 +0100

    0

--- qqspace_darkcave.js
+++ qqspace_darkcave.js
@@ -67,8 +67,8 @@ loader.load('hurtmice.glb', function (gltf) {
     pointLight.position.set(0, 5, 0);
     scene.add(pointLight);
 
-    model.position.set(0, 0, 0);
-    model.scale.set(0.1, 0.1, 0.1);
+    model.position.set(0, 0, -10);
+    model.scale.set(0.3, 0.3, 0.3);
     scene.add(model);
 
     // ✅ 添加一个小模型副本（放在摄像机前方）
